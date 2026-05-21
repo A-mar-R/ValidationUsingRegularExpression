91bd089705d5e2cc6265f9bd28a9778fd4ae78e2
A-ma-R <amarjadhav9022@gmail.com>
2026-05-21 23:14:47 +0530

    Add project files.

---
+++ ValidationUsingRegularExpression/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ValidationUsingRegularExpression.Models;
+
+namespace ValidationUsingRegularExpression.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(Employee emp)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(emp);
+            }
+            return View("Details", emp);
+        }
+    }
+}
---
+++ ValidationUsingRegularExpression/Models/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ValidationUsingRegularExpression.Models
+{
+    public class Employee
+    {
+        [Required(ErrorMessage = "First Name is required.")]
+        public string?  FirstName { get; set; }
+        [Required(ErrorMessage = "Last Name is required.")] 
+        public string? LastName { get; set; }
+        [Required(ErrorMessage = "Designation is required.")]
+        public  string? Designation { get; set; }
+
+        [Required(ErrorMessage = "Email Address is required.")]
+        [RegularExpression("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", ErrorMessage = "Invalid email address.")]
+        public  string? EmailAddress { get; set; }
+        
+        [Required(ErrorMessage = "Mobile Number is required.")]
+        [RegularExpression("^[0-9]{10}$", ErrorMessage = "Mobile number must be exactly 10 digits.")]
+        public long MobileNumber { get; set; }
+        [Required(ErrorMessage = "Age is required.")]
+        public int? EmployeeAge { get; set; }
+
+        [Required(ErrorMessage = "Date of Birth is required.")]
+        [DataType(DataType.Date, ErrorMessage = "Invalid date format.")]
+        public DateOnly? DateOfBirth { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [RegularExpression("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$", ErrorMessage = "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character.")]
+        public string? Password { get; set; }
+    }
+}
